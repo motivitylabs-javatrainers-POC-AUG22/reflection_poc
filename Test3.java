fe141b63b0e895939a8260eb203b3e4d4aea9417
krishnagolla <107397978+krishnagolla@users.noreply.github.com>
2022-08-16 12:37:17 +0530

    Add files via upload
    .class syntax

---
+++ Test3.java
@@ -0,0 +1,11 @@
+package refl_apii;
+
+class Test3{  
+	  public static void main(String args[]){  
+	   Class c = boolean.class;   
+	   System.out.println(c.getName());  
+	  
+	   Class c2 = Test3.class;   
+	   System.out.println(c2.getName());  
+	 }  
+	}  
